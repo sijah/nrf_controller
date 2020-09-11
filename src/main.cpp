bde0ae1cdefaca0dea655bdc8d59e766660c703e
sijah.ak <sijah.ak@gmail.com>
2020-09-11 21:54:40 +0530

    nrf test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
-#include <Wire.h>
-
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
 void setup() {
-  // put your setup code here, to run once:
-  //test
-  //int
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));
+  delay(1000);
 }
